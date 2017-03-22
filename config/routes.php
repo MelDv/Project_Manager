33ad1d6622fdf8007c0a7035b11a9ee1f2f5b34d
Maaret Dufva <maaret.dufva@gmail.com>
2017-03-22 19:10:30 +0200

    navbar kunnossa, lisätty kirjautumislomake ja omien tietojen muokkauslomake

--- config/routes.php
+++ config/routes.php
@@ -27,3 +27,11 @@ $routes->get('/projektit/1', function() {
 $routes->get('/tehtava', function() {
     HelloWorldController::tehtava();
 });
+
+$routes->get('/kirjaudu', function() {
+    HelloWorldController::kirjaudu();
+});
+
+$routes->get('/muokkaa_omasivu', function() {
+    HelloWorldController::muokkaa_omasivu();
+});
