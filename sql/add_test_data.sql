f586d2e8ced3d353e63edf01301bc3c0a922c453
Maaret Dufva <maaret.dufva@gmail.com>
2017-03-23 17:31:21 +0200

    tietokantataulut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO job (name, current_status, short_description) VALUES ('Web-sovellus', 'underway', 'Tehdään Bootstrap-projektinhallintasovellus');
+INSERT INTO person (name, password, sotu, email, phone, current_rights) VALUES ('Kalle', 'Kalle123', '11111111111', 'kalle@koti.home', 0988833322, 'admin');
+INSERT INTO project (job, manager) VALUES (1, 1);
+INSERT INTO task (owner_project) VALUES (1);
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,49 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TYPE rights AS ENUM ('admin', 'worker', 'intern', 'visitor');
+CREATE TYPE status AS ENUM('pending', 'underway', 'finished');
+
+CREATE TABLE person(
+    id SERIAL PRIMARY KEY,
+    name varchar(40) NOT NULL,
+    password varchar(40) NOT NULL,
+    sotu character(11),
+    address varchar(120),
+    email varchar(40) NOT NULL,
+    phone INTEGER NOT NULL,
+    workphone INTEGER,
+    office varchar(10),
+    title varchar(40),
+    description varchar(2000),
+    active BOOLEAN DEFAULT TRUE,
+    current_rights rights DEFAULT 'visitor'
+);
+
+CREATE TABLE job(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    current_status status DEFAULT 'pending',
+    late BOOLEAN DEFAULT FALSE,
+    short_description varchar(400) NOT NULL,
+    description varchar(5000),
+    start_date DATE,
+    deadline DATE,
+    creator INTEGER REFERENCES person(id),
+    approved BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE project (
+    job INTEGER REFERENCES job PRIMARY KEY,
+    manager INTEGER REFERENCES person(id)
+);
+
+CREATE TABLE task (
+    job INTEGER REFERENCES job PRIMARY KEY,
+    owner_project varchar(50) NOT NULL
+);
+
+-- liitostaulu tehtävän ja tekijöiden väliin; workforcella voi olla yksi tehtävä, monta tekijää
+CREATE TABLE workforce (
+    id SERIAL PRIMARY KEY,
+    owner_task INTEGER REFERENCES task(job),
+    worker INTEGER REFERENCES person(id)
+);
