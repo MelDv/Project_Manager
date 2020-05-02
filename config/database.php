e8be3e1c716e8197182ca85930f53040edb8511c
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 15:13:37 +0300

    composer changes

--- config/database.php
+++ config/database.php
@@ -2,33 +2,33 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
-//
-//class DatabaseConfig {
-//
-//    private static $use_database = 'psql';
-//    private static $connection_config = array(
-//        'psql' => array(
-//            'resource' => 'pgsql:'
-//        ),
-//        'mysql' => array(
-//            'resource' => 'mysql:unix_socket=/home/KAYTTAJATUNNUS/mysql/socket;dbname=mysql',
-//            'username' => 'root',
-//            'password' => 'SALASANA'
-//        )
-//    );
-//
-//    public static function connection_config() {
-//        $config = array(
-//            'db' => self::$use_database,
-//            'config' => self::$connection_config[self::$use_database]
-//        );
-//        return $config;
-//    }
-//
-//}
-//
-//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//$cleardb_server = $cleardb_url["host"];
-//$cleardb_username = $cleardb_url["user"];
-//$cleardb_password = $cleardb_url["pass"];
-//$cleardb_db = substr($cleardb_url["path"], 1);
+
+class DatabaseConfig {
+
+    private static $use_database = 'psql';
+    private static $connection_config = array(
+        'psql' => array(
+            'resource' => 'pgsql:'
+        ),
+        'mysql' => array(
+            'resource' => 'mysql:unix_socket=/home/KAYTTAJATUNNUS/mysql/socket;dbname=mysql',
+            'username' => 'root',
+            'password' => 'SALASANA'
+        )
+    );
+
+    public static function connection_config() {
+        $config = array(
+            'db' => self::$use_database,
+            'config' => self::$connection_config[self::$use_database]
+        );
+        return $config;
+    }
+
+}
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
--- config/environment.sh
+++ config/environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-USERNAME="postgres"
+USERNAME="svzldcojgyipsi"
 PATH="postgres://svzldcojgyipsi:7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5@ec2-54-210-128-153.compute-1.amazonaws.com:5432/dcnmle7k63eh3"
 DATABASE_URL="postgresql-defined-97990"
