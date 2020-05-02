7f6315e6615c109da0e3103317e1841a6e186b63
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 18:22:52 +0300

    db fix

--- config/database.php
+++ config/database.php
@@ -26,9 +26,3 @@ class DatabaseConfig {
     }
 
 }
-
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
