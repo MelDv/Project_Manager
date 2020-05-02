e04a22db70fd30b7d8191cf1b658021ad9b730f7
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 19:20:58 +0300

    db variables changed

--- config/environment.sh
+++ config/environment.sh
@@ -2,9 +2,10 @@
 
 DB_USERNAME="svzldcojgyipsi"
 DB_PATH="postgres://svzldcojgyipsi:7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5@ec2-54-210-128-153.compute-1.amazonaws.com:5432/dcnmle7k63eh3"
-DATABASE_URL="postgresql-defined-97990"
+DATABASE_URL="postgres://svzldcojgyipsi:7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5@ec2-54-210-128-153.compute-1.amazonaws.com:5432/dcnmle7k63eh3"
 DB_HOST=postgres
 DB_CONNECTION=pgsql
 DB_PORT=5432
 DB_DATABASE=default
 DB_PASSWORD=1234
+DB_HOST=postgres
