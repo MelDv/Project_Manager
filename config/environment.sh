cde3dbf0b298defc0553887fd3eae2963a70fd85
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 15:01:28 +0300

    database access

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="madufva"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
-# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
+USERNAME="postgres"
+PATH="postgres://svzldcojgyipsi:7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5@ec2-54-210-128-153.compute-1.amazonaws.com:5432/dcnmle7k63eh3"
+DATABASE_URL="postgresql-defined-97990"
