5694f2fe3665a4e3d2d369e5b966d11fab5e9483
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 19:02:33 +0300

    Changed db conf

--- config/database.php
+++ config/database.php
@@ -3,26 +3,34 @@
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 
-class DatabaseConfig {
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
-    private static $use_database = 'psql';
-    private static $connection_config = array(
-        'psql' => array(
-            'resource' => 'pgsql:'
-        ),
-        'mysql' => array(
-            'resource' => 'mysql:unix_socket=/home/KAYTTAJATUNNUS/mysql/socket;dbname=mysql',
-            'username' => 'root',
-            'password' => 'SALASANA'
-        )
-    );
 
-    public static function connection_config() {
-        $config = array(
-            'db' => self::$use_database,
-            'config' => self::$connection_config[self::$use_database]
-        );
-        return $config;
-    }
+$active_group = 'default';
+$query_builder = TRUE;
 
-}
+$db['default'] = array(
+    'dsn' => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
