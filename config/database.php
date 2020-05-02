6246644bf285685ddd74518898eb78954571b7fe
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 19:26:40 +0300

    db conf

--- config/database.php
+++ config/database.php
@@ -3,13 +3,6 @@
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-
-
 $active_group = 'default';
 $query_builder = TRUE;
 
