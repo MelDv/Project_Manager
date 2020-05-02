990931a6914de2c681f070e5f1b06fc2c72ba2aa
MelDv <maaret.dufva@helsinki.fi>
2020-05-02 19:09:44 +0300

    db connection fixing...

--- config/database.php
+++ config/database.php
@@ -34,3 +34,29 @@ $db['default'] = array(
     'failover' => array(),
     'save_queries' => TRUE
 );
+
+class DatabaseConfig
+{
+
+    private static $use_database = 'psql';
+    private static $connection_config = array(
+        'psql' => array(
+            'resource' => 'pgsql:'
+        ),
+        'mysql' => array(
+            'resource' => 'DB_PATH;DB_NAME',
+            'username' => 'DB_USERNAME',
+            'password' => 'DB_PASSWORD'
+        )
+    );
+
+    public static function connection_config()
+    {
+        $config = array(
+            'db' => self::$use_database,
+            'config' => self::$connection_config[self::$use_database]
+        );
+        return $config;
+    }
+
+}
