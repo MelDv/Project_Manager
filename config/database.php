62ae660f9ca67fe5ec279705e026f77fcafdb714
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 12:12:08 +0300

    pdo fixed

--- config/database.php
+++ config/database.php
@@ -6,9 +6,9 @@ class DatabaseConfig
     public static function connection_config()
     {
         $config = array(
-            'port' => '5432',
-            'host' => 'ec2-54-210-128-153.compute-1.amazonaws.com',
             'dbname' => 'dcnmle7k63eh3',
+            'host' => 'ec2-54-210-128-153.compute-1.amazonaws.com',
+            'port' => '5432',
             'user' => 'svzldcojgyipsi',
             'password' => '7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5',
             'sslmode' => 'require'
--- lib/database.php
+++ lib/database.php
@@ -14,20 +14,20 @@ class DB
         // Haetaan tietokantakonfiguraatio
         $connection_config = DatabaseConfig::connection_config();
         $config = array(
-            'port' => '5432',
-            'host' => 'ec2-54-210-128-153.compute-1.amazonaws.com',
             'dbname' => 'dcnmle7k63eh3',
+            'host' => 'ec2-54-210-128-153.compute-1.amazonaws.com',
+            'port' => '5432',
             'user' => 'svzldcojgyipsi',
             'password' => '7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5',
             'sslmode' => 'require'
         );
 
-        try {
+        try {$pdoConnection = new PDO("pgsql:host=hostname;dbname=databasename;user=username;password=thepassword");
             // Alustetaan PDO
-            if (isset($config['username'])) {
-                $connection = new PDO($config['port'], $config['host'], $config['dbname'], $config['user'], $config['password'], $config['sslmode']);
+            if (isset($config['user'])) {
+                $connection = new PDO("pgsql:host=ec2-54-210-128-153.compute-1.amazonaws.com;dbname=dcnmle7k63eh3;user=svzldcojgyipsi;password=7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5");
             } else {
-                $username = $config['username'];
+                $username = $config['user'];
                 $connection = new PDO($config['port']);
             }
             // Asetetaan tietokannan kenttien koodaukseksi utf8
