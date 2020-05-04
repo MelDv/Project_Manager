1cf17cc6f2cc786e43fadec12aa3bc405df69968
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 11:19:36 +0300

    config changes

--- config/database.php
+++ config/database.php
@@ -3,25 +3,14 @@
 
 class DatabaseConfig {
 
-    private static $use_database = 'psql';
-    private static $connection_config = array(
-        'psql' => array(
-            'resource' => 'pgsql:'
-        ),
-        'mysql' => array(
+    public static function connection_config() {
+        $config = array(
             'port' => 5432,
             'host'=> 'ec2-54-210-128-153.compute-1.amazonaws.com',
             'dbname' => 'svzldcojgyipsi',
             'username' => 'svzldcojgyipsi',
             'password' => '7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5',
             'sslmode' => 'require'
-        )
-    );
-
-    public static function connection_config() {
-        $config = array(
-            'db' => self::$use_database,
-            'config' => self::$connection_config[self::$use_database]
         );
         return $config;
     }
