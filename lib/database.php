b02e305bb303c4c666dca5c504b3675965caeed0
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 12:28:34 +0300

    twig method corrected for newer version

--- lib/database.php
+++ lib/database.php
@@ -1,9 +1,4 @@
 <?php
-$db = parse_url(getenv("DATABASE_URL"));
-$db["path"] = ltrim($db["path"], "/");
-
-$active_group = 'default';
-$query_builder = TRUE;
 
 class DB
 {
@@ -11,17 +6,6 @@ class DB
 
     public static function connection()
     {
-        // Haetaan tietokantakonfiguraatio
-        $connection_config = DatabaseConfig::connection_config();
-        $config = array(
-            'dbname' => 'dcnmle7k63eh3',
-            'host' => 'ec2-54-210-128-153.compute-1.amazonaws.com',
-            'port' => '5432',
-            'user' => 'svzldcojgyipsi',
-            'password' => '7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5',
-            'sslmode' => 'require'
-        );
-
         try {
             $connection = new PDO("pgsql:host=ec2-54-210-128-153.compute-1.amazonaws.com;dbname=dcnmle7k63eh3;user=svzldcojgyipsi;password=7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5");
             // Asetetaan tietokannan kenttien koodaukseksi utf8
