e066c9537ace3a49c1e7dae0dbdddd3eea0804a3
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 12:16:33 +0300

    removed error

--- lib/database.php
+++ lib/database.php
@@ -23,8 +23,6 @@ class DB
         );
 
         try {
-            $pdoConnection = new PDO("pgsql:host=hostname;dbname=databasename;user=username;password=thepassword");
-
             $connection = new PDO("pgsql:host=ec2-54-210-128-153.compute-1.amazonaws.com;dbname=dcnmle7k63eh3;user=svzldcojgyipsi;password=7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5");
             // Asetetaan tietokannan kenttien koodaukseksi utf8
             $connection->exec('SET NAMES UTF8');
@@ -34,7 +32,7 @@ class DB
             $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
         } catch (PDOException $e) {
-            die('Virhe tietokantayhteydessä tai tietokantakyselyssä: username: ' . $e->getMessage());
+            die('Virhe tietokantayhteydessä tai tietokantakyselyssä: ' . $e->getMessage());
         }
 
         return $connection;
