8d613222e1c31b6e85253cf998ec4a72280ed5fb
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 10:59:53 +0300

    dbconfig

--- config/database.php
+++ config/database.php
@@ -3,23 +3,9 @@
 
 class DatabaseConfig {
 
-    private static $use_database = 'psql';
-    private static $connection_config = array(
-        'psql' => array(
-            'resource' => 'pgsql:'
-        ),
-        'mysql' => array(
-            'resource' => 'mysql:unix_socket=/home/DB_USERNAME/mysql/socket;dbname=mysql',
-            'username' => 'DB_USERNAME',
-            'password' => 'DB_PASSWORD'
-        )
-    );
-
     public static function connection_config() {
-        $config = array(
-            'db' => self::$use_database,
-            'config' => self::$connection_config[self::$use_database]
-        );
+        $config = "dbname=dcnmle7k63eh3 host=ec2-54-210-128-153.compute-1.amazonaws.com port=5432 user=svzldcojgyipsi password=7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5 sslmode=require";
         return $config;
     }
 }
+//$con = "dbname=dcnmle7k63eh3 host=ec2-54-210-128-153.compute-1.amazonaws.com port=5432 user=svzldcojgyipsi password=7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5 sslmode=require";
