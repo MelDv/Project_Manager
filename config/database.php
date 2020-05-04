57b17946c72aeb5222666def4c32151656c543c4
MelDv <maaret.dufva@helsinki.fi>
2020-05-04 11:38:50 +0300

    changed wrond dbname

--- config/database.php
+++ config/database.php
@@ -11,7 +11,7 @@ class DatabaseConfig {
         'mysql' => array(
             'port' => '5432',
             'host'=> 'ec2-54-210-128-153.compute-1.amazonaws.com',
-            'dbname' => 'svzldcojgyipsi',
+            'dbname' => 'dcnmle7k63eh3',
             'username' => 'svzldcojgyipsi',
             'password' => '7bb6b2b5352e1dbf13a6a1a4781e4fe1b777b37ab73305147aa1c674685a80f5',
             'sslmode' => 'require'
